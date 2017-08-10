dd318aa60da0cc402b88e046f6662b348037fb5f
j <kingjak678@gmail.com>
2017-08-10 15:02:28 +0000

    update

--- receiver.py
+++ receiver.py
@@ -5,19 +5,21 @@ import websockets
 APP_URI = 'wss://violctrl.herokuapp.com'
 DEV_URI = 'ws://10.0.0.201:3000'
 
-# async def handler(socket):
-
 async def connect():
     async with websockets.connect(DEV_URI) as socket:
         # of course don't actually hardcode this
         connectMsg = {'type': 'receiver_connect', 'ruid': 'peyote', 'key': 'wile.e.'}
         await socket.send(json.dumps(connectMsg))
         while True:
-            message = await socket.recv()
-            print(message) 
+            msg = await socket.recv()
+            msg = json.loads(msg)
+            handleMessage(msg) 
 
         # task = asyncio.ensure_future(handler(socket))
         # done, pending = await asyncio.wait([task], return_when=asyncio.FIRST_COMPLETED)
+
+def handleMessage(msg):
+
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(connect())
